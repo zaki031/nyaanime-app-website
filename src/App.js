fb5738cd4275c2e8498666abc1d9976299a7e6df
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-02-07 19:03:47 +0100

    site build !

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import mockup from './mockupp.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-background h-full  p-9">
+      <div className="hh flex justify-between">
+        <div className="circle w-[600px]  h-[600px] rounded-[50%] bg-main">
+        <img src={mockup} className="h-[650px] ml-[10%]" alt="" />
+
+        </div>
+
+
+        <div className="txt  mt-[10%]  mr-[10%]">
+          <h1 className="text-main text-[7rem]">Nyaanime</h1>
+          <p className="text-main  text-3xl  ">
+            Latest anime episodes totally for free.
+          </p>
+          <br />
+          <br />
+          <div className="btn  text-center ">
+            <a
+              href="https://expo.dev/artifacts/eas/5QSisHgjJ8y4EK5CA7Xqyb.apk"
+              className="bg-main place-self-center hover:shadow-xl rounded-xl p-3 text-background text-4xl mt-[10%]"
+            >
+              Download Now!
+            </a>
+            <br /><br />
+            <p className="text-light">"available for androind only" <i class="uil uil-android"></i> </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+     "background":"#1b1c21",
+     "main":"#a888ff",
+     "light":"#fff",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
