036f3d61016f8ef93a6b506564b424136bc64b9e
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-02-07 19:43:44 +0100

    top svg and mockup adjusments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import mockup from './mockupp.png'
+import mockup from './mockuppp.png'
 function App() {
   return (
-    <div className="App bg-background h-full  p-9">
-      <div className="hh flex justify-between">
-        <div className="circle w-[600px]  h-[600px] rounded-[50%] bg-main">
-        <img src={mockup} className="h-[650px] ml-[10%]" alt="" />
+    <div className="App bg-background  m-0 h-[100vh]  ">
+     <div class="custom-shape-divider-top-1675793784">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" className="h-[45px] w-full" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" class="shape-fill fill-main "></path>
+    </svg>
+</div>
+      <div className="hh mt-[50px]   flex justify-between">
+        <div className="circle w-[600px] justify-start align-left  h-[600px] rounded-[50%] bg-main">
+        <img src={mockup} className="h-[658px] ml-[50px] mb-[50px]  " alt="" />
 
         </div>
 
@@ -25,7 +30,7 @@ function App() {
             >
               Download Now!
             </a>
-            <br /><br />
+            <br /><br /><br />
             <p className="text-light">"available for androind only" <i class="uil uil-android"></i> </p>
           </div>
         </div>
